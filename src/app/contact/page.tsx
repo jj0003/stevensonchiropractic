1499c515a66f102e38bab43425823701d1579419
jj0003 <janfuriocatalan@protonmail.com>
2024-11-04 16:37:36 +1100

    Update Contact page: Change allowFullScreen attribute to use JSX syntax

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -117,7 +117,7 @@ export default function Contact() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509419!2d144.95373531531593!3d-37.81627927975152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d43f1f6e1bb%3A0x5045675218ce7e0!2sNewcastle!5e0!3m2!1sen!2sau!4v1629341234567!5m2!1sen!2sau"
             width="100%"
             height="100%"
-            allowFullScreen=""
+            allowFullScreen={true}
             loading="lazy"
             className="border-0"
             title="Newcastle Chiropractic Center Location"
